2d72b099be67742a207039c2684e8b3998f54dd8
Alex Secundino <alex.secundino@gmail.com>
2014-03-26 13:11:47 +0100

    Añadir características del juego

--- JuegoROL/src/Principal.java
+++ JuegoROL/src/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*Crear juego. Cartas tienen ataque/defensa/PuntosInvocacion*/
+		/*Cuando matas a todas las criaturas del oponente ganas*/
+		/*Cada ronda te dan 2 puntos que servirán para invocar carta*/
+		
 	}
 
 }
